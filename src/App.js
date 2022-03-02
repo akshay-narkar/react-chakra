3265a9e80db78056d8f55b944fc3fe26b7d0c55c
akshay-narkar <akshaynarkar1295@gmail.com>
2022-03-02 18:58:11 +0530

    Setup react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// import './App.css';
+// import { Switch } from '@chakra-ui/react'
+// import { Stack, HStack, VStack } from '@chakra-ui/react'
+import BootstrapSwitchButton from 'bootstrap-switch-button-react'
+import { Box, Image, Flex, Badge, Text } from "@chakra-ui/react";
+import { MdStar } from "react-icons/md";
+
+ import { PhoneIcon, AddIcon, WarningIcon } from '@chakra-ui/icons'
+
+
+
+export default function Example() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box p="5" maxW="320px" borderWidth="1px">
+      <Image borderRadius="md" src="https://bit.ly/2k1H1t6" />
+      <Flex align="baseline" mt={2}>
+        <Badge colorScheme="pink">Plus</Badge>
+        <Text
+          ml={2}
+          textTransform="uppercase"
+          fontSize="sm"
+          fontWeight="bold"
+          color="pink.800"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Verified &bull; Cape Town
+        </Text>
+      </Flex>
+      <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
+        Modern, Chic Penthouse with Mountain, City & Sea Views
+      </Text>
+      <Text mt={2}>$119/night</Text>
+      <Flex mt={2} align="center">
+        <Box as={MdStar} color="orange.400" />
+        <Text ml={1} fontSize="sm">
+          <b>4.84</b> (190)
+        </Text>
+      </Flex>
+    </Box>
   );
 }
 
-export default App;
+
+// function App() {
+//   return (
+//     <>
+//     <PhoneIcon />
+
+// // Use the `boxSize` prop to change the icon size
+// <AddIcon w={6} h={6} />
+
+// // Use the `color` prop to change the icon color
+// <WarningIcon w={8} h={8} color="red.500" />
+//   <BootstrapSwitchButton checked={true} size="lg" />
+
+//       <PhoneIcon/>
+//     <div className="App">
+//       Akshay
+       
+//  </div>
+//  </>
+//   );
+// }
+
+// export default App;
+
+// const initstate = {
+//   teams: [],
+//   teaminfo: [],
+// };
+
+// const store = createStore(rootReducer, {
+//   teamupdatereducer: initstate,
+//   teamdeetsReducer: initstate,
+// }, applyMiddleware(thunk));
+
+// store.dispatch(apicall);
+
+// export default App;
